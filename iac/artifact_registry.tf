a4ade50489719613d216194ba644b87c39fe1afd
MattBarkway <contact@mattbarkway.dev>
2025-10-24 10:27:10 +0100

    just migration image for faster CI

--- iac/artifact_registry.tf
+++ iac/artifact_registry.tf
@@ -4,7 +4,6 @@ resource "google_artifact_registry_repository" "summit_stats_repo" {
   repository_id      = "summit-stats-repo"
   format             = "DOCKER"
   description        = "Docker repo for SummitStats backend images"
-  cleanup_policy_dry_run = true
 
   cleanup_policies {
     id     = "keep-5-most-recent"
@@ -22,7 +21,6 @@ resource "google_artifact_registry_repository" "summit_stats_frontend_repo" {
   repository_id      = "summit-stats-frontend-repo"
   format             = "DOCKER"
   description        = "Docker repo for SummitStats frontend images"
-  cleanup_policy_dry_run = true
 
   cleanup_policies {
     id     = "keep-5-most-recent"
@@ -40,7 +38,6 @@ resource "google_artifact_registry_repository" "summit_stats_migrations_repo" {
   repository_id      = "summit-stats-migrations-repo"
   format             = "DOCKER"
   description        = "Docker repo for SummitStats frontend images"
-  cleanup_policy_dry_run = true
 
   cleanup_policies {
     id     = "keep-5-most-recent"
