9bbc22259617fc9013cedc98f3e7b3eebb17804e
MattBarkway <contact@mattbarkway.dev>
2025-10-24 19:51:09 +0100

    update path

--- iac/bootstrap/vpc.tf
+++ iac/bootstrap/vpc.tf
@@ -16,7 +16,7 @@ resource "google_vpc_access_connector" "cloud_run_connector" {
   name   = "cloud-run-connector"
   region = var.region
   network = "default"
-  ip_cidr_range = "10.8.0.0/28"  # small range for connector
+  ip_cidr_range = "10.8.0.0/28"
   min_instances = 2
   max_instances = 3
 }
